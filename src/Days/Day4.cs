44f9985d1deac0111c7a6572590b05f6887ea551
Blake Farrugia <bmfarrugia@gmail.com>
2017-12-06 23:31:03 -0500

    Day4 Part2

--- src/Days/Day4.cs
+++ src/Days/Day4.cs
@@ -53,7 +53,36 @@ namespace Advent.Days
 
         private string[] DoPartB()
         {
-            return new string[]{};
+            var count = 0;
+            var isValid = true;
+            foreach (var pass in this._buffer)
+            {
+                var words = pass.Split(' ');
+
+                for (var i = 0; i < words.Length - 1 && isValid; i++)
+                {
+                    var sortedWord = SortWord(words[i]);
+                    for (var j = i + 1; j < words.Length && isValid; j++)
+                    {
+                        isValid = isValid && (sortedWord != SortWord(words[j]));
+                    }
+                }
+
+                if (isValid)
+                {
+                    count++;
+                }
+                isValid = true;
+            }
+
+            return new string[]{count.ToString()};
+        }
+
+        private string SortWord(string word)
+        {
+            var chars = word.ToCharArray();
+            Array.Sort(chars);
+            return new string(chars);
         }
     }
 }

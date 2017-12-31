fbbcc69395c6d3ef2ecb470368a508ec4797d41f
Blake Farrugia <bmfarrugia@gmail.com>
2017-12-31 02:48:52 -0500

    Day9 Part2

--- src/Days/Day9.cs
+++ src/Days/Day9.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace Advent.Days
 {
     class Day9 : IDay
@@ -7,18 +5,13 @@ namespace Advent.Days
         private string[] _buffer;
         private bool _negate;
         private bool _garbageEnabled;
+        private int _garbageCount;
         private int _score;
-        private Stack<char>[] _stacks;
 
         public Day9(string[] inputs)
         {
             this._buffer = inputs;
-            this._negate = this._garbageEnabled = false;
-            this._stacks = new Stack<char>[]
-            {
-                new Stack<char>(),
-                new Stack<char>()
-            };
+            this.Reset();
         }
 
         public void PrintResults()
@@ -28,6 +21,7 @@ namespace Advent.Days
                 System.Console.WriteLine(result);
             }
 
+            this.Reset();
             foreach(var result in DoPartB())
             {
                 System.Console.WriteLine(result);
@@ -36,7 +30,21 @@ namespace Advent.Days
 
         private string[] DoPartA()
         {
-            var inputs = this._buffer[0].ToCharArray();
+            this.ParseInput(this._buffer[0]);
+
+            return new string[]{this._score.ToString()};
+        }
+
+        private string[] DoPartB()
+        {
+            this.ParseInput(this._buffer[0], true);
+
+            return new string[]{(this._garbageCount).ToString()};
+        }
+
+        private void ParseInput(string line, bool collectGarbage = false)
+        {
+            var inputs = line.ToCharArray();
             var groupLevel = 0;
             foreach (var input in inputs)
             {
@@ -53,35 +61,51 @@ namespace Advent.Days
                         case '{':
                         if (!this._garbageEnabled)
                         {
-                            this._stacks[0].Push(input);
                             groupLevel++;
                             this._score += groupLevel;
+                        }
+                        else if (collectGarbage)
+                        {
+                            this._garbageCount++;
                         }
                         break;
                         case '}':
                         if (!this._garbageEnabled)
                         {
-                            this._stacks[0].Pop();
                             groupLevel--;
+                        } 
+                        else if (collectGarbage)
+                        {
+                            this._garbageCount++;
                         }
                         break;
-                        case '<': this._stacks[1].Push(input);
-                        this._garbageEnabled = true;
+                        case '<': 
+                        if (!this._garbageEnabled)
+                        {
+                            this._garbageEnabled = true;
+                        }
+                        else if (collectGarbage)
+                        {
+                            this._garbageCount++;
+                        }
                         break;
-                        case '>': this._stacks[1].Pop();
-                        this._garbageEnabled = false;
+                        case '>': this._garbageEnabled = false;
                         break;
-                        default: break;
+                        default:
+                        if (collectGarbage && this._garbageEnabled)
+                        {
+                            this._garbageCount++;
+                        }
+                        break;
                     }
                 }
             }
-
-            return new string[]{this._score.ToString()};
         }
 
-        private string[] DoPartB()
+        private void Reset()
         {
-            return new string[]{};
+            this._garbageCount = this._score = 0;
+            this._garbageEnabled = this._negate = false;
         }
     }
 }
